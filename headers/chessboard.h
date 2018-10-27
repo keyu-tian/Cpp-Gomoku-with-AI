0603f917f88e3c9900ba51ec922c2068d43468e7
Kevin <K_code@163.com>
2018-10-27 19:46:24 +0800

    Update chessboard.h

--- headers/chessboard.h
+++ headers/chessboard.h
@@ -8,22 +8,24 @@ class ChessBoard
 	private:
 
 		Chessid black_player_id;
-		Chessid white_player_id;
 		Chessid board[GRID_N][GRID_N];
 
 	public:
+
 		ChessBoard();
 
-		void init();
+		void initBoard();
 		void putChess(int x, int y, Chessid id);
 		void takeChess(int x, int y);
+		Chessid getChessID(int x, int y) const;
 
+		void setBlackPlayerID(Chessid id);
+		Chessid getBlackPlayerID() const;
+
+		bool isInside(int r, int c) const;
 		bool isInside(const Grid &c) const;
 		bool isAvaliable(const Grid &c) const;
 		bool win(const Grid &just_now, const Chessid id);
-		Chessid getChessID(int x, int y) const;
-		Chessid getBlackID() const;
-		Chessid getWhiteID() const;
 
 };
 
