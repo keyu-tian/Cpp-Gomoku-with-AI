95997d6d575fb592fbc356ad69c97cd955d75cf0
Kevin <K_code@163.com>
2018-10-27 19:52:33 +0800

    Update and rename mypainter.h to game_painter.h

---
+++ headers/game_painter.h
@@ -0,0 +1,25 @@
+#ifndef GAME_PAINTER_H
+#define GAME_PAINTER_H
+
+#include <QBrush>
+#include <QColor>
+#include <QPainter>
+#include <QPen>
+
+#include "base.h"
+#include "chess_board.h"
+#include "ui_size_param.h"
+
+
+class GamePainter : public QPainter
+{
+	public:
+		GamePainter(QPaintDevice *parent = Q_NULLPTR);
+		void init(void);
+		void drawGrid(void);
+		void drawMark(const Grid &c, bool is_black);
+		void drawCross(const Grid &c, bool is_black);
+		void drawChess(const ChessBoard &c);
+};
+
+#endif // GAME_PAINTER_H
