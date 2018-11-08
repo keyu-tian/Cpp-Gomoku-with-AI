8b27bef222c25c171e6c0fc3a6333d2f100bb08a
Kevin <K_code@163.com>
2018-11-08 23:49:14 +0800

    definition of the class GamePainter

--- sources/game_painter.cpp
+++ sources/game_painter.cpp
@@ -1,53 +1,67 @@
-#include "mypainter.h"
-
+#include "game_painter.h"
 #include <QTime>
 
 enum BW
 {
 	BLACK1 = 58,
 	BLACK2 = 64,
-	WHITE1 = 238,
+	WHITE1 = 238,//239
 	WHITE2 = 244
 };
 
-MyPainter::MyPainter(QPaintDevice *parent)
+GamePainter::GamePainter(QPaintDevice *parent)
 	: QPainter(parent)
 {
 
 }
 
-void MyPainter::init()
+void GamePainter::init()
 {
 	this->setPen(Qt::black);
 	this->setRenderHint(QPainter::Antialiasing, true);	// 抗锯齿
 }
 
-void MyPainter::drawBoard()
+void GamePainter::drawGrid()
 {
 	for(int i=BOARD_L; i<=BOARD_R; i+=GRID_W)
 	{
 		this->drawLine(BOARD_L, i, BOARD_R, i);	// 水平线
 		this->drawLine(i, BOARD_L, i, BOARD_R);	// 竖直线
 	}
-	this->drawRect(BOARD_L-10, BOARD_L-10, BOARD_W + 20, BOARD_W + 20);
+	this->drawRect(BOARD_L-7, BOARD_L-7, BOARD_W+14, BOARD_W+14);
 }
 
-void MyPainter::drawMark(const Grid &c)
+void GamePainter::drawMark(const Grid &c, bool is_black)
 {
 	QRadialGradient radi;
+	if (is_black)
+		radi.setColorAt(0,qRgb(BLACK1, BLACK1, BLACK1));
+	else
+		radi.setColorAt(0,qRgb(WHITE1, WHITE1, WHITE1));
 
-//	if (game->playerFlag)
-//		radi.setColorAt(0,qRgb(WHITE1, WHITE1, WHITE1));
-//	else
-		radi.setColorAt(0,qRgb(BLACK2, BLACK2, BLACK2));
 	this->setBrush(radi);
+	this->setPen(Qt::black);
 	this->drawRect(BOARD_L + GRID_W * c.x + GRID_W - MARK_R,
 			 BOARD_L + GRID_W * c.y + GRID_W - MARK_R,
 			 2 * MARK_R, 2 * MARK_R);
-	// 以上 +GRID_W 是因为 棋格坐标（0,0）-> 屏幕坐标（GRID_W, GRID_W）
+	// 以上 +GRID_W 是因为 棋格坐标（0, 0） <=> 屏幕坐标（GRID_W, GRID_W）
 }
 
-void MyPainter::drawChess(const ChessBoard &c)
+void GamePainter::drawCross(const Grid &c, bool is_black)
+{
+	QRadialGradient radi;
+	if (is_black)
+		radi.setColorAt(0,qRgb(BLACK1, BLACK1, BLACK1));
+	else
+		radi.setColorAt(0,qRgb(WHITE1, WHITE1, WHITE1));
+
+	this->setBrush(radi);
+	this->drawEllipse(BOARD_L + GRID_W * c.x + GRID_W - MARK_R,
+			 BOARD_L + GRID_W * c.y + GRID_W - MARK_R,
+			 2 * MARK_R, 2 * MARK_R);
+}
+
+void GamePainter::drawChess(const ChessBoard &c)
 {
 	this->setPen(Qt::NoPen);
 	QRadialGradient radi;
@@ -57,13 +71,14 @@ void MyPainter::drawChess(const ChessBoard &c)
 		for (int j=0; j<GRID_N; j++)
 		{
 			Chessid now_id = c.getChessID(i, j);
+			Chessid black_player_id = c.getBlackPlayerID();
 			if (now_id != NO_CHESS)
 			{
-				if (now_id == AI_CHESS)
+				if (now_id == black_player_id)
 				{
 //					radi.setColorAt(0,qRgb(WHITE2, WHITE2, WHITE2));
 //					this->setBrush(radi);
-//					this->drawEllipse(BOARD_UB + GRID_W * (i+1) - CHESS_R-2, BOARD_UB + GRID_W * (j+1) - CHESS_R-2, (CHESS_R+2) * 2, (CHESS_R+2) * 2);
+//					this->drawEllipse(BOARD_U + GRID_W * (i+1) - CHESS_R-2, BOARD_U + GRID_W * (j+1) - CHESS_R-2, (CHESS_R+2) * 2, (CHESS_R+2) * 2);
 
 					radi.setColorAt(0,qRgb(BLACK1, BLACK1, BLACK1));
 					this->setBrush(radi);
@@ -73,7 +88,7 @@ void MyPainter::drawChess(const ChessBoard &c)
 				{
 //					radi.setColorAt(0,qRgb(WHITE2, WHITE2, WHITE2));
 //					this->setBrush(radi);
-//					this->drawEllipse(BOARD_UB + GRID_W * (i+1) - CHESS_R-1, BOARD_UB + GRID_W * (j+1) - CHESS_R-1, (CHESS_R+1) * 2, (CHESS_R+1) * 2);
+//					this->drawEllipse(BOARD_U + GRID_W * (i+1) - CHESS_R-1, BOARD_U + GRID_W * (j+1) - CHESS_R-1, (CHESS_R+1) * 2, (CHESS_R+1) * 2);
 
 					radi.setColorAt(0,qRgb(BLACK2, BLACK2, BLACK2));
 					this->setBrush(radi);
@@ -87,5 +102,4 @@ void MyPainter::drawChess(const ChessBoard &c)
 		}
 	}
 }
-
 
