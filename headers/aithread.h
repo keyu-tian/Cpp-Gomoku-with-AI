b9491551d57c22f908321329c8c7392b74d2fd10
Kevin <K_code@163.com>
2019-03-01 22:35:51 +0800

    declaration of the class AIThread
    declaration of the class AIThread

--- headers/aithread.h
+++ headers/aithread.h
@@ -15,14 +15,14 @@ class AiThread : public QThread
 		int round;
 		bool is_black;
 		bool warned;
-		Grid ai_next_move;
+		Grid ai_next_move; // 未初始化
 
 		int prev_score;
 
-		// 棋盘的备份（为了更快读写）
+		// 棋盘的备份（为了更快读写）（初始化置0）
 		Chessid mirror_board[GRID_N][GRID_N];
 
-		/* 记录某条直线上的棋子，每2个bit表示一颗棋子
+		/* 记录某条直线上的棋子，每2个bit表示一颗棋子（初始化置0）
 		 *  列：最左的是第0列，每列靠上的是低位，扫描次数10
 		 *  行：最上的是第0行，每行靠左的是低位，扫描次数10
 		 *  左斜（左上右下）：最左下的是第0左斜列，靠左上的是低位，扫描次数与位置有关
@@ -33,21 +33,24 @@ class AiThread : public QThread
 		__int32 left_chess[GRID_DN];
 		__int32 right_chess[GRID_DN];
 
-		// 记录每条直线上的棋型，每 1 个 __int32 表示一条直线上的棋型
+		// 记录每条直线上的棋型，每 1 个 __int32 表示一条直线上的棋型（初始化置0）
 		__int32 col_type[GRID_N];
 		__int32 row_type[GRID_N];
 		__int32 left_type[GRID_DN];
 		__int32 right_type[GRID_DN];
 
-		// 记录整个棋盘的棋型，每 1 个 int 表示一种棋型的个数
+		// 记录整个棋盘的棋型，每 1 个 int 表示一种棋型的个数（初始化置0）
 		int all_type[CHESS_TYPE_N];
 
-		// 棋型标志位映射表
+		// 棋型标志位映射表（初始化置0）
 		int M_TABLE[MAX_INDEX_OF_M_TABLE];
-		int P4_KEY_POS_TABLE[MAX_INDEX_OF_M_TABLE];
-		int A3_KEY_POS_TABLE[MAX_INDEX_OF_M_TABLE][3];
 
-		// 获取斜列扫描次数映射表
+		// 关键位置映射表（初始化置-1）
+		char P4_KEY_POS_TABLE[MAX_INDEX_OF_M_TABLE];		// 冲四的关键点位置（落子成五：1个位置）
+		char A3_KEY_POS_TABLE[MAX_INDEX_OF_M_TABLE][3];		// 活三的关键点位置（落子成活四/冲四：2+1=3个位置）
+		char S3_KEY_POS_TABLE[MAX_INDEX_OF_M_TABLE][2];		// 眠三的关键点位置（落子成冲四：2个位置）
+
+		// 获取斜列扫描次数映射表（初始化置0）
 		int XY_TO_LEFT_COUNT[GRID_N][GRID_N];
 		int XY_TO_RIGHT_COUNT[GRID_N][GRID_N];
 		int L_TO_LEFT_COUNT[GRID_DN];
@@ -74,11 +77,15 @@ class AiThread : public QThread
 		// 棋盘接口函数
 		void putChess(const int x, const int y, const Chessid id);
 		void takeChess(const int x, const int y);
-		bool win();
-		bool lose();
+		bool win(void) const;
+		bool lose(void) const;
 		void printChessType();
+		void algoDebuging(void) const;
 
-		void findP4(int &x, int &y, const Chessid cur_player);
+		// 寻找敌方唯一的P4
+		int findEnemyP4(Choice *choices_buffer, const Chessid cur_player) const;
+		int findS3A3(Choice *choices_buffer, const Chessid cur_player) const;
+		int findAllS3A3(Choice *choices_buffer, const Chessid cur_player) const;
 
 	private:
 
@@ -91,16 +98,17 @@ class AiThread : public QThread
 
 		// 估值函数：
 		int evalBoard();
-		int evalMinMaxPrior(const int x, const int y, const Chessid id);
-		int evalKillPrior(const int x, const int y, const Chessid id);
-		int evalKilledScore(Chessid cur_player, int depth);
+		int evalMinMaxPrior(const int x, const int y, const Chessid id) const;
+		int evalKillPrior(const int x, const int y, const Chessid id) const;
+		int evalKilledScore(Chessid cur_player, int depth) const;
 
 		// 算法函数：
-		int minMaxSearch(Chessid cur_player, int depth, int alpha, int beta);
-		int killSearch(Chessid cur_player, int depth, int alpha, int beta);
-		int getMinMaxSearchChoices(Choice *choices_buffer, Chessid cur_player);
-		int getKillSearchChoices(Choice *choices_buffer, Chessid cur_player);
-		void myQuickSort(Choice *a, int n);
+		int minMaxSearch(const Chessid cur_player, const int depth, int alpha, int beta);
+		int killSearch(const Chessid cur_player, const int depth, int alpha, int beta);
+		int resSearch(const Chessid cur_player, const int depth, int alpha, int beta);
+		int getMinMaxSearchChoices(Choice *choices_buffer, Chessid cur_player) const;
+		int getKillSearchChoices(Choice *choices_buffer, Chessid cur_player) const;
+		void myQuickSort(Choice *a, int n) const;
 
 		// 特殊局面函数：
 		void firstRound();
@@ -112,6 +120,7 @@ class AiThread : public QThread
 
 		// 线程间信号传输
 		void foundNextMove(const Grid &ai_next_move);
+		void findingNextMove(const Grid &ai_next_move);
 		void sweetWarning();
 
 };
