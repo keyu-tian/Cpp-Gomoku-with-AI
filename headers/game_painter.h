ff56c690409a7e4503aa1a8c68c655ef5335b309
Kevin <K_code@163.com>
2019-03-01 22:40:47 +0800

    declaration of the class GamePainter
    declaration of the class GamePainter

--- headers/game_painter.h
+++ headers/game_painter.h
@@ -10,16 +10,21 @@
 #include "chess_board.h"
 #include "ui_size_param.h"
 
+
 class GamePainter : public QPainter
 {
 	public:
 
 		GamePainter(QPaintDevice *parent = Q_NULLPTR);
+
+		// 注意绘图的顺序：网格点->关键位点->鼠标标记点->AI预落子点->棋子->上次落子点
 		void init(void);
 		void drawGrid(void);
+		void drawKeyPos(const Grid &c, bool is_P4);
 		void drawMark(const Grid &c, bool is_black);
-		void drawCross(const Grid &c, bool is_black);
+		void drawAIPreMove(const Grid &c);
 		void drawChess(const ChessBoard &c);
+		void drawLastMove(const Grid &c, bool is_black);
 };
 
 #endif // GAME_PAINTER_H
